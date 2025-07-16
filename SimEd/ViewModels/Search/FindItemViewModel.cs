22ffdddf986c9ad7219cc084b378ccd43dfcd727
Ciprian Khlud <ciprian.mustiata@gmail.com>
2025-07-16 14:23:57 +0300

    Adds a non-SVG icon

--- SimEd/ViewModels/Search/FindItemViewModel.cs
+++ SimEd/ViewModels/Search/FindItemViewModel.cs
@@ -1,3 +1,4 @@
+using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using SimEd.Models.Languages.Common;
 
@@ -5,9 +6,11 @@ namespace SimEd.ViewModels.Search;
 
 public class FindItemViewModel : ObservableObject
 {
-    public string FileName  =>GetFileName();
+    public string FileName => GetFileName();
 
-    private string GetFileName() => $"{SolutionItem.Token}:{SolutionItem.Kind}";
+    private string GetFileName() => $"{SolutionItem.Token.GetText()}";
 
     public SolutionIndexItem SolutionItem { get; set; }
+    
+    //public Bitmap? ImageType { get; } = ImageHelper.LoadFromResource(new Uri("avares://Assets/Icons/csharp-original.png"));
 }
