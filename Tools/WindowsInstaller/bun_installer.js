ec6699297de0743bc5a8d7e2a0bd63034f47a712
Ciprian Khlud <ciprian.mustiata@gmail.com>
2025-07-17 23:48:29 +0300

    Preparing the installer for 0.0.4

--- Tools/WindowsInstaller/bun_installer.js
+++ Tools/WindowsInstaller/bun_installer.js
@@ -1,11 +1,12 @@
-﻿import {$} from "bun";
+﻿#!bun
+import {$} from "bun";
 
-const projectPath = 'C:\\oss\\SimEd'
+const ProjectPath = 'C:\\oss\\SimEd'
 
 const InnoSetupCompiler = `C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe`
 
 async function buildProgramOnPlatform(platform) {
-    await $`dotnet publish  ..\\..\\SimEd\\SimEd.csproj -r ${platform} -c Release -o ${platform} -p:PublishDir=.\\${platform}`;
+    await $`dotnet publish  ..\\..\\SimEd\\SimEd.csproj -r ${platform} -c Release -o ${platform} -p:PublishDir=.\\${platform}`
     await $`${InnoSetupCompiler} installer-${platform}.iss`
     await $`mv ..\\tools\\${platform}\\mysetup.exe setup-simaed-${platform}.exe`
 }
