ad75f1732142c3b0e61f11000b938ed97c0c2d95
Ciprian Khlud <ciprian.mustiata@gmail.com>
2025-07-18 23:47:25 +0300

    Fixes versioning to not be copy-pasta.

---
+++ SimEd/HelpAboutWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SimEd.ViewModels.Documents;
+
+namespace SimEd;
+
+public class HelpAboutWindowViewModel : BaseFileViewModel
+{
+    public string VersionValue => GetVersionString();
+
+    private string GetVersionString()
+    {
+        try
+        {
+            var assembly = typeof(HelpAboutWindowViewModel).Assembly;
+
+            var version = assembly.GetName().Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+        catch
+        {
+            return "0.0.0-UNKNOWN";
+        }
+    }
+}
