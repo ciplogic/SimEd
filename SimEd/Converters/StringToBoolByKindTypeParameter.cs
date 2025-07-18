945abab77781ee04589050a9bfc103d05065e93e
iDomas <domo.emailas@gmail.com>
2025-07-18 20:30:21 +0300

    more icons

--- SimEd/Converters/StringToBoolByKindTypeParameter.cs
+++ SimEd/Converters/StringToBoolByKindTypeParameter.cs
@@ -6,7 +6,15 @@ namespace SimEd.Converters;
 public class StringToBoolByKindTypeParameter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => Equals(value, parameter);
+    {
+        if (parameter is string str && value is string target)
+        {
+
+            return str.Split(',').Any(x => Equals(x, target));
+        }
+
+        return false;
+    }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
