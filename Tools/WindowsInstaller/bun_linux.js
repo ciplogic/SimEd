2d4e137e46b522e604332e5540371aa427e04865
ciplogic <ciprian.mustiata@gmail.com>
2025-07-20 21:54:23 +0300

    Add Linux script

---
+++ Tools/WindowsInstaller/bun_linux.js
@@ -0,0 +1,15 @@
+﻿#!bun
+import {$} from "bun";
+
+const SimEdVersion = "0.0.5"
+
+async function buildProgramOnPlatform(platform) {
+    await $`rm -rf ${platform}`
+    await $`dotnet publish  ..\\..\\SimEd\\SimEd.csproj -r ${platform} -c Release -o ${platform} -p:PublishDir=.\\${platform}`
+    var zipFileName = `simed-${SimEdVersion}-${platform}.zip`
+    await $`rm -f ${zipFileName}`
+    await $`zip -r ${zipFileName} ${platform}`
+    await $`rm -rf ${platform}`
+}
+
+await buildProgramOnPlatform("linux-x64");
